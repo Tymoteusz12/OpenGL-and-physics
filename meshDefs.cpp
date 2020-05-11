d23bef16d19ee69d493fe59ce2367dfb2dff0d3a
Tymoteusz12 <55797064+Tymoteusz12@users.noreply.github.com>
2020-05-11 14:03:38 +0200

    Revert "Add project to repository"
    This reverts commit 3992ac07271fdb73278f874609a2f8b7f1a0f413.

--- meshDefs.cpp
+++
@@ -1,9 +0,0 @@
-#include "mesh.h"
-
-Mesh::Mesh(vector<Vertex>vertices, vector<unsigned int>indices, vector<Texture>textures):
-	vertices(vertices),
-	indices(indices),
-	textures(textures) {
-	
-	Mesh::setupMesh();
-}
